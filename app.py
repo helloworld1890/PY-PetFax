96143ea7aef61f10e45b4a9ca4f36f6b0a2630ee
Craig Caparaz <cappuccino1890@Craigs-Air.lan1>
2022-06-26 22:10:39 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask 
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Hello, this is PetFax!'
+
+@app.route('/pets')
+def pets():
+    return 'These are our pets avaialable for adoption!'
