fa84e0b908b947402e0e123fff59cb9cde117603
Craig Caparaz <cappuccino1890@Craigs-Air.lan1>
2022-06-30 23:36:26 -0700

    commit seven

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,10 +1,20 @@
 from flask import (Blueprint, redirect, render_template, request, redirect)
+from . import models
 
 bp4  = Blueprint('fact', __name__, url_prefix="/facts" )
 
 @bp4.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-         print(request.form)
-         return redirect('/facts')    
-    return render_template('facts/index.html')
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+        
+        new_fact = models.Fact(submitter=submitter, fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
+        return redirect('/facts')
+    results = models.Fact.query.all()
+    
+
+    return render_template('facts/index.html', facts=results)
